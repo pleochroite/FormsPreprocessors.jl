10fe2fbb83f5057b5f50fca82e7b5becec5f0cf7
schigy <sychulc@me.com>
2022-03-04 02:38:10 +0900

    added apply_dict

--- src/FormsPreprocessors.jl
+++ src/FormsPreprocessors.jl
@@ -1,5 +1,20 @@
 module FormsPreprocessors
+using CSV, DataFrames, DataFramesMeta, Missings, Parameters
 
-# Write your package code here.
+function apply_dict(dict, x)
+    if ismissing(x)
+        missing    
+    elseif x ∈ keys(dict)
+        dict[x]
+    else
+        x
+    end
+end
 
+function convert_answer!(df, key, dict)
+    @transform!(df, $key = map(x -> apply_dict(dict, x), $key))
+end
+
+
+export apply_dict, convert_answer!
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,22 @@
 using FormsPreprocessors
 using Test
+using DataFrames, DataFramesMeta
 
-@testset "FormsPreprocessors.jl" begin
-    # Write your tests here.
+df = DataFrame(bt = ["A", "A", "O", "AB", "B", "B", "C"],
+    rh = ["+", "-", "+", "+", "+", "+", "+"])
+bloodtype = Dict(["A" => "1", "B" => "2", "O" => "3", "AB" => "4"])
+
+
+@testset "SurveyPreprocessors.jl" begin
+    @testset "convert_answers!" begin
+        @test apply_dict(bloodtype, "A") == "1"
+        @test apply_dict(bloodtype, "B") == "2"
+        @test apply_dict(bloodtype, "O") == "3"
+        @test apply_dict(bloodtype, "AB") == "4"
+        @test apply_dict(bloodtype, "K") == "K"
+        @test apply_dict(bloodtype, 'A') == 'A'
+        @test apply_dict(bloodtype, :A) == :A
+        @test apply_dict(bloodtype, 0) == 0
+        @test apply_dict(bloodtype, missing) |> ismissing == true
+    end
 end
