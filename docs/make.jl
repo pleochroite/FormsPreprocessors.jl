b3abade0392b3cf32b8700cc995873d531272985
schigy <sychulc@me.com>
2022-03-12 01:38:59 +0900

    updated documenter-related stuffs

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,9 @@
 using Documenter, FormsPreprocessors
 
-makedocs(sitename="FormsPreprocessors")
+makedocs(
+    sitename = "FormsPreprocessors",
+    pages = Any[
+        "Home" => "index.md",
+        "References" => "references.md"
+    ],
+    modules = [FormsPreprocessors])
